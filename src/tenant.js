377986c3cc36b081a13eebd5bcee584f670712f9
Ryan Berger <rberger@ebsco.com>
2026-05-13 17:08:32 -0400

    Sonar fix

--- src/tenant.js
+++ src/tenant.js
@@ -1,7 +1,17 @@
 
+const optionalString = (source, field) => {
+  const value = source[field];
+  if (value === undefined || value === null) return undefined;
+  if (typeof value !== 'string') {
+    throw new TypeError(`${field} must be a string`);
+  }
+
+  return value;
+};
+
 export default class Tenant {
   constructor(name, clientId) {
-    this.name = name;
-    this.clientId = clientId;
+    this.name = optionalString(name, 'name');
+    this.clientId = optionalString(clientId, 'clientId');
   }
 }
