53f3b78b681cec34b142ca574d6e0726f30b7c31
Ryan Berger <rberger@ebsco.com>
2026-05-22 11:47:06 -0400

    Add App.jsx wrapper

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {
+  useState,
+  useEffect,
+} from 'react';
+import PropTypes from 'prop-types';
+
+import { IntlProvider } from 'react-intl';
+import { QueryClientProvider } from 'react-query';
+
+import { loadTranslations } from './loadTranslations';
+import { isValidConfig } from './loginServices';
+import createReactQueryClient from './createReactQueryClient';
+import Router from './Router';
+import ConfigError from './ConfigError';
+import configuration from '../config.yaml';
+
+const App = () => {
+  const config = configuration.config || {};
+  const branding = configuration.branding || {};
+  const location = globalThis.location;
+  const locale = config.locale || navigator.language || 'en-US';
+  
+  const reactQueryClient = createReactQueryClient();
+  const Component = isValidConfig(config) ?
+    <Router branding={branding} config={config} location={location} />
+    :
+    <ConfigError branding={branding} config={config} />
+
+  const [translations, setTranslations] = useState([]);
+
+  useEffect(() => {
+    loadTranslations(locale).then((msgs) => setTranslations(msgs));
+  }, [locale]);
+
+  return (
+    <QueryClientProvider client={reactQueryClient}>
+      <IntlProvider locale={locale} messages={translations} >
+        {Component}
+      </IntlProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { IntlProvider } from 'react-intl';
-import { QueryClientProvider } from 'react-query';
 
-import { loadTranslations } from './loadTranslations';
-import { isValidConfig } from './loginServices';
-import createReactQueryClient from './createReactQueryClient';
-import Router from './Router';
-import ConfigError from './ConfigError';
-import configuration from '../config.yaml';
-
-const config = configuration.config || {};
-const branding = configuration.branding || {};
-const location = globalThis.location;
-const locale = config.locale || navigator.language || 'en-US';
-const translations = await loadTranslations(locale);
-const reactQueryClient = createReactQueryClient();
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const Component = isValidConfig(config) ?
-  <Router branding={branding} config={config} location={location} />
-  :
-  <ConfigError branding={branding} config={config} />
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={reactQueryClient}>
-      <IntlProvider locale={locale} messages={translations} >
-        {Component}
-      </IntlProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
